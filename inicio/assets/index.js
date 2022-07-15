590eaf2058d2e2d161b98169cebba74d93a53d95
VictorHugoChavez <96146923+VictorHugoChavez@users.noreply.github.com>
2022-07-15 10:02:32 -0500

    se suben repsaso de javascript

---
+++ inicio/assets/index.js
@@ -0,0 +1,17 @@
+var nombre;
+nombre = "Hola mundo";
+var puntuacion = 2;
+var record;
+record = 6000;
+alert(5 + 6 * 3);
+
+var precuoUnidad, cantidad, total;
+
+precuoUnidad = 24;
+cantidad = 100;
+total = precuoUnidad * cantidad;
+
+alert("El total de mi valores es " + total);
+
+
+var nombre = prompt("Intro")
